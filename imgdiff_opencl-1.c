e04e46909654b3c89e74cb85b6680a04d1d0ba86
Виктория Шангина <viktoriasangina@Viktoria-MacBook-Pro.local>
2016-02-25 14:31:52 +0900

    new file img_opencl-1.c

--- imgdiff_opencl-1.c
+++ imgdiff_opencl-1.c
@@ -120,130 +120,6 @@ void save_binary(const char* file_name,	cl_program program, cl_uint num_devs)
 }
 
 
-//TODO : Unblock clBuildProgram
-static void build_program_callback(cl_program program, void *user_data) {
-    cl_int err;
-    cl_build_status build_status;
-    bp_data_t *bp_data = (bp_data_t *)user_data;
-    
-    // Check the build status.
-    err = clGetProgramBuildInfo(program, bp_data->dev,
-                                CL_PROGRAM_BUILD_STATUS,
-                                sizeof(cl_build_status),
-                                &build_status, NULL);
-    printf("%s\n",TranslateOpenCLError(err));
-    if (build_status != CL_BUILD_SUCCESS) {
-        exit(EXIT_FAILURE);
-    }
-    
-    // Set the event status
-    err = clSetUserEventStatus(*(bp_data->event), CL_COMPLETE);
-    
-    printf("%s\n",TranslateOpenCLError(err));
-    
-    /*
-     cl_int* err;
-     err = (cl_int*)malloc(sizeof(cl_int)*1);
-     cl_build_status build_status;
-     bp_data_t *bp_data = (bp_data_t *)user_data;
-     int i;
-     // Check the build status.
-     for( i = 0; i < 1; i++ )
-     {
-     err[i] = clGetProgramBuildInfo(program, bp_data->dev[i],
-     CL_PROGRAM_BUILD_STATUS,
-     sizeof(cl_build_status),
-     &build_status, NULL);
-     printf("%s\n",TranslateOpenCLError(err[i]));
-     if (build_status != CL_BUILD_SUCCESS) {
-     //	print_build_log(program, bp_data->dev);
-     exit(EXIT_FAILURE);
-     }
-     // Set the event status
-     err[i] = clSetUserEventStatus(*(bp_data->event), CL_COMPLETE);
-     printf("%s\n",TranslateOpenCLError(err[i]));
-     }
-     free(err);
-     */
-}
-
-
-//TODO : Translate OpenCLerror
-static const char* TranslateOpenCLError(cl_int errorCode)
-{
-    switch (errorCode)
-    {
-        case CL_SUCCESS:                            return "CL_SUCCESS";
-        case CL_DEVICE_NOT_FOUND:                   return "CL_DEVICE_NOT_FOUND";
-        case CL_DEVICE_NOT_AVAILABLE:               return "CL_DEVICE_NOT_AVAILABLE";
-        case CL_COMPILER_NOT_AVAILABLE:             return "CL_COMPILER_NOT_AVAILABLE";
-        case CL_MEM_OBJECT_ALLOCATION_FAILURE:      return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
-        case CL_OUT_OF_RESOURCES:                   return "CL_OUT_OF_RESOURCES";
-        case CL_OUT_OF_HOST_MEMORY:                 return "CL_OUT_OF_HOST_MEMORY";
-        case CL_PROFILING_INFO_NOT_AVAILABLE:       return "CL_PROFILING_INFO_NOT_AVAILABLE";
-        case CL_MEM_COPY_OVERLAP:                   return "CL_MEM_COPY_OVERLAP";
-        case CL_IMAGE_FORMAT_MISMATCH:              return "CL_IMAGE_FORMAT_MISMATCH";
-        case CL_IMAGE_FORMAT_NOT_SUPPORTED:         return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
-        case CL_BUILD_PROGRAM_FAILURE:              return "CL_BUILD_PROGRAM_FAILURE";
-        case CL_MAP_FAILURE:                        return "CL_MAP_FAILURE";
-        case CL_MISALIGNED_SUB_BUFFER_OFFSET:       return "CL_MISALIGNED_SUB_BUFFER_OFFSET";                              //-13
-        case CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST:    return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_L    IST";   //-14
-            /*
-             33                                                                  case CL_COMPILE_PROGRAM_FAILURE:            re    turn "CL_COMPILE_PROGRAM_FAILURE";                               //-15
-             34                                                                  case CL_LINKER_NOT_AVAILABLE:               re    turn "CL_LINKER_NOT_AVAILABLE";                                  //-16
-             35                                                                  case CL_LINK_PROGRAM_FAILURE:               re    turn "CL_LINK_PROGRAM_FAILURE";                                  //-17
-             36                                                                  case CL_DEVICE_PARTITION_FAILED:            re    turn "CL_DEVICE_PARTITION_FAILED";                               //-18
-             37                                                                  case CL_KERNEL_ARG_INFO_NOT_AVAILABLE:      re    turn "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";                         //-19
-             38                                                                */
-        case CL_INVALID_VALUE:                      return "CL_INVALID_VALUE";
-        case CL_INVALID_DEVICE_TYPE:                return "CL_INVALID_DEVICE_TYPE";
-        case CL_INVALID_PLATFORM:                   return "CL_INVALID_PLATFORM";
-        case CL_INVALID_DEVICE:                     return "CL_INVALID_DEVICE";
-        case CL_INVALID_CONTEXT:                    return "CL_INVALID_CONTEXT";
-        case CL_INVALID_QUEUE_PROPERTIES:           return "CL_INVALID_QUEUE_PROPERTIES";
-        case CL_INVALID_COMMAND_QUEUE:              return "CL_INVALID_COMMAND_QUEUE";
-        case CL_INVALID_HOST_PTR:                   return "CL_INVALID_HOST_PTR";
-        case CL_INVALID_MEM_OBJECT:                 return "CL_INVALID_MEM_OBJECT";
-        case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:    return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
-        case CL_INVALID_IMAGE_SIZE:                 return "CL_INVALID_IMAGE_SIZE";
-        case CL_INVALID_SAMPLER:                    return "CL_INVALID_SAMPLER";
-        case CL_INVALID_BINARY:                     return "CL_INVALID_BINARY";
-        case CL_INVALID_BUILD_OPTIONS:              return "CL_INVALID_BUILD_OPTIONS";
-        case CL_INVALID_PROGRAM:                    return "CL_INVALID_PROGRAM";
-        case CL_INVALID_PROGRAM_EXECUTABLE:         return "CL_INVALID_PROGRAM_EXECUTABLE";
-        case CL_INVALID_KERNEL_NAME:                return "CL_INVALID_KERNEL_NAME";
-        case CL_INVALID_KERNEL_DEFINITION:          return "CL_INVALID_KERNEL_DEFINITION";
-        case CL_INVALID_KERNEL:                     return "CL_INVALID_KERNEL";
-        case CL_INVALID_ARG_INDEX:                  return "CL_INVALID_ARG_INDEX";
-        case CL_INVALID_ARG_VALUE:                  return "CL_INVALID_ARG_VALUE";
-        case CL_INVALID_ARG_SIZE:                   return "CL_INVALID_ARG_SIZE";
-        case CL_INVALID_KERNEL_ARGS:                return "CL_INVALID_KERNEL_ARGS";
-        case CL_INVALID_WORK_DIMENSION:             return "CL_INVALID_WORK_DIMENSION";
-        case CL_INVALID_WORK_GROUP_SIZE:            return "CL_INVALID_WORK_GROUP_SIZE";
-        case CL_INVALID_WORK_ITEM_SIZE:             return "CL_INVALID_WORK_ITEM_SIZE";
-        case CL_INVALID_GLOBAL_OFFSET:              return "CL_INVALID_GLOBAL_OFFSET";
-        case CL_INVALID_EVENT_WAIT_LIST:            return "CL_INVALID_EVENT_WAIT_LIST";
-        case CL_INVALID_EVENT:                      return "CL_INVALID_EVENT";
-        case CL_INVALID_OPERATION:                  return "CL_INVALID_OPERATION";
-        case CL_INVALID_GL_OBJECT:                  return "CL_INVALID_GL_OBJECT";
-        case CL_INVALID_BUFFER_SIZE:                return "CL_INVALID_BUFFER_SIZE";
-        case CL_INVALID_MIP_LEVEL:                  return "CL_INVALID_MIP_LEVEL";
-        case CL_INVALID_GLOBAL_WORK_SIZE:           return "CL_INVALID_GLOBAL_WORK_SIZE";                               //-63
-        case CL_INVALID_PROPERTY:                   return "CL_INVALID_PROPERTY";                                       //-64
-            /*
-             75                                                        case CL_INVALID_IMAGE_DESCRIPTOR:           return "CL_I    NVALID_IMAGE_DESCRIPTOR";                           //-65
-             76                                                        case CL_INVALID_COMPILER_OPTIONS:           return "CL_I    NVALID_COMPILER_OPTIONS";                           //-66
-             77                                                        case CL_INVALID_LINKER_OPTIONS:             return "CL_I    NVALID_LINKER_OPTIONS";                             //-67
-             78                                                        case CL_INVALID_DEVICE_PARTITION_COUNT:     return "CL_I    NVALID_DEVICE_PARTITION_COUNT";                     //-68
-             79                                                      */
-            //    case CL_INVALID_PIPE_SIZE:                  return "C    L_INVALID_PIPE_SIZE";                                  //-69
-            //    case CL_INVALID_DEVICE_QUEUE:               return "C    L_INVALID_DEVICE_QUEUE";                               //-70
-        default:
-            return "UNKNOWN ERROR CODE";
-    }
-}
-
-
 int ReadSourceFromFile(const char* fileName, char** source, size_t* sourceSize)
 {
     int errorCode = CL_SUCCESS;
@@ -298,7 +174,7 @@ void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigne
     
     int err = CL_SUCCESS;
     // binary index
-    int use_binary = 1;//check where it used ---------------------
+    int use_binary = 1;
     
     int i, j;
     
